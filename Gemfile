357dae7fbbd62e79b46d647b608bd404f8f4ca2a
Julian Smith <smith.julian.f@gmail.com>
2019-11-29 17:46:06 +0000

    Implemented ImageMagick (and MiniMagick) to scale uploaded images

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'devise'
 gem 'carrierwave', '~> 2.0'
 gem 'figaro'
 gem 'fog-aws'
+gem 'mini_magick'
 
 
 # Use ActiveStorage variant
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
 	config.fog_provider = 'fog/aws'                        # required
 	config.fog_credentials = {
 	  provider:              'AWS',                        # required
-	  aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-	  aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+	  aws_access_key_id:     ENV['AWS_ACCESS_KEY'],        # required
+	  aws_secret_access_key: ENV['AWS_SECRET_KEY'],        # required
 	}
-	config.fog_directory  = ENV["AWS_BUCKET"]              # required
+	config.fog_directory  = ENV['AWS_BUCKET']              # required
   end
