a3c799531f4bb7967ab477b2ea913d246253a0bf
Julian Smith <smith.julian.f@gmail.com>
2019-12-01 21:37:05 +0000

    Made it so that users cannot enter a lesson for a course that they are not enrolled in

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_user_be_enrolled, only: [:show]
+
     def show
     end
 
@@ -8,6 +11,12 @@ class LessonsController < ApplicationController
 
     def current_lesson
         @current_lesson ||= Lesson.find(params[:id])
+    end
+
+    def require_user_be_enrolled
+        if !current_user.enrolled_in?(current_lesson.section.course)
+            redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in this course before you can view its lessons.'
+        end
     end
 
 end
