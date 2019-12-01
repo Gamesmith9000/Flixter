61828d1ac734b497c4bacd6fc73410e9218135b2
Julian Smith <smith.julian.f@gmail.com>
2019-12-01 02:36:31 +0000

    Set up student 'enrollments' for classes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,12 +1,13 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
+  has_many :enrollments
 
   mount_uploader :image, ImageUploader
 
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
-# It seems like :image should also be validated (to exists, at least) since it is a required (?) field in our form in views/instructor/courses/new.html.erb
+# It seems like :image should also be validated (to exist, at least) since it is a required (?) field in our form in views/instructor/courses/new.html.erb
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'static_pages#index'
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :courses, only: [:index, :show]
   resources :lessons, only: [:show]
   namespace :instructor do
